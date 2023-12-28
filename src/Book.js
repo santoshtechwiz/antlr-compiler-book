2cc7ad0260dec889a8cdccd0f28dd143803fd531
Santosh Kumar Singh <SANSI@orsted.com>
2023-12-28 13:37:38 +0800

    Fix folder structure

--- src/Book.js
+++ src/Book.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import "./Book.css"; // Create a separate CSS file for styling
 
 // @ts-ignore
@@ -15,14 +16,17 @@ import Chapter2 from "./chapter2.md";
 const Chapter = ({ title, content }) => (
   <section className="book-chapter" data-aos="fade-up">
     <h2 className="title">{title}</h2>
-    <div className="stackedit" dangerouslySetInnerHTML={{ __html: content }} />
+    <div
+      className="stackedit"
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
   </section>
 );
 
 const Book = () => {
   const [introContent, setIntroContent] = useState("");
-  const [chapter, setchapter] = useState("");
-  const [chapter1, setchapter1] = useState("");
+  const [chapter, setChapter] = useState("");
+  const [chapter1, setChapter1] = useState("");
 
   useEffect(() => {
     AOS.init({
@@ -39,23 +43,43 @@ const Book = () => {
       .catch((error) => console.error("Error fetching intro.md:", error));
     fetch(Chapter1)
       .then((response) => response.text())
-      .then((data) => setchapter(data))
-      .catch((error) => console.error("Error fetching intro.md:", error));
-      fetch(Chapter2)
+      .then((data) => setChapter(data))
+      .catch((error) => console.error("Error fetching chapter1.md:", error));
+    fetch(Chapter2)
       .then((response) => response.text())
-      .then((data) => setchapter1(data))
-      .catch((error) => console.error("Error fetching intro.md:", error));
+      .then((data) => setChapter1(data))
+      .catch((error) => console.error("Error fetching chapter2.md:", error));
   }, []);
 
   return (
-    <div>
-      <Chapter title="" content={introContent}></Chapter>
-      <Chapter title="Chapter 1" content={chapter}></Chapter>
-      <Chapter title="Chapter 2" content={chapter1}></Chapter>
+    <Router>
+      <div className="book-container">
+        <nav className="book-nav">
+          <NavLink to="/">Introduction</NavLink>
+          <NavLink to="/chapter1">Chapter 1</NavLink>
+          <NavLink to="/chapter2">Chapter 2</NavLink>
+        </nav>
+        <div className="book-content">
+          <Routes>
+            <Route
+              path="/"
+              element={<Chapter title="Introduction" content={introContent} />}
+            />
+            <Route
+              path="/chapter1"
+              element={<Chapter title="Chapter 1" content={chapter} />}
+            />
+            <Route
+              path="/chapter2"
+              element={<Chapter title="Chapter 2" content={chapter1} />}
+            />
+          </Routes>
+        </div>
+      </div>
       <section className="book-buy" data-aos="fade-up">
         <button>Buy Now</button>
       </section>
-    </div>
+    </Router>
   );
 };
 
